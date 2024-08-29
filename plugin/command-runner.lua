bbb55e6452ce740a0d481579a0f08b0f293fdd87
marzeq <marzeqmarzeq@gmail.com>
2024-08-29 16:19:28 +0200

    fix

--- plugin/command-runner.lua
+++ plugin/command-runner.lua
@@ -3,7 +3,7 @@ vim.api.nvim_create_user_command("CommandRunnerRunAll", function()
   require("command-runner").run_command(nil)
 end, {})
 vim.api.nvim_create_user_command("CommandRunnerRun", function(args)
-  require("command-runner").run_command(tonumber(args[1]))
+  require("command-runner").run_command(tonumber(args.fargs[1]))
 end, {
   nargs = 1,
 })
