8a441c564f7be374a6a713551ee3a40a2ec325e2
marzeq <marzeqmarzeq@gmail.com>
2024-08-30 20:43:40 +0200

    redr backend: fix client:recieve blocking main thread

--- lua/backends/redr.lua
+++ lua/backends/redr.lua
@@ -1,6 +1,7 @@
 ---@diagnostic disable: deprecated
 local M = require("command-runner")
 local socket = require("socket")
+local uv = vim.loop
 
 local MAX_BYTES = 16 * 1024
 local REDR_PORT = 45673
@@ -45,24 +46,39 @@ local function parse_message(msg)
   end
 end
 
----@param client unknown @socket.unix
----@param msg string @message to send to the server
----@return string|nil, unknown|nil @response from the server
-local function send_message(client, msg)
-  local success, err = client:send(msg)
+---@param client uv_tcp_t @The TCP client (a uv_tcp_t handle)
+---@param msg string @Message to send to the server
+---@param callback function @Callback to handle the response or error
+local function send_message(client, msg, callback)
+  client:write(msg, function(write_err)
+    if write_err then
+      callback(nil, write_err)
+      return
+    end
 
-  if not success then
-    return nil, err
-  end
+    local buf = ""
+    local read_cb
 
-  ---@diagnostic disable-next-line: redefined-local
-  local response, err = client:receive("*l")
+    read_cb = function(err, chunk)
+      if err then
+        callback(nil, err)
+        return
+      end
 
-  if not response then
-    return nil, err
-  end
+      if chunk then
+        buf = buf .. chunk
+        -- Since we're expecting a newline-terminated message
+        if buf:find("\n") then
+          client:read_stop()
+          callback(buf, nil)
+        end
+      else
+        callback(nil, "Connection closed")
+      end
+    end
 
-  return response, nil
+    client:read_start(read_cb)
+  end)
 end
 
 ---@param commands string[]
@@ -75,75 +91,81 @@ local function run_command(commands, cwd)
     end
   end
 
-  local client = socket.tcp4()
-  if not client then
-    vim.notify("Error connecting to redr server", vim.log.levels.ERROR)
-    client:close()
-    return
-  end
-
-  local success = client:connect("127.0.0.1", tostring(REDR_PORT))
-  if not success then
-    vim.notify("Error connecting to redr server", vim.log.levels.ERROR)
-    client:close()
-    return
-  end
-
-  local introduce, introduce_err = send_message(client, introduce_message(cwd))
-  if introduce_err ~= nil then
-    vim.notify("Error introducing to redr server: " .. vim.inspect(introduce_err), vim.log.levels.ERROR)
-    client:close()
-    return
-  end
-
-  ---@diagnostic disable-next-line: param-type-mismatch
-  local introduce_parsed = parse_message(introduce)
-
-  if introduce_parsed == nil or introduce_parsed.type ~= "ok" then
-    vim.notify("Error parsing response from redr server: " .. vim.inspect(introduce), vim.log.levels.ERROR)
-    send_message(client, bye_message())
-    client:close()
-    return
-  end
-
-  for _, command in ipairs(commands) do
-    local msg = run_command_message(command)
-
-    local res, err = send_message(client, msg)
-
-    if err ~= nil then
-      vim.notify("Error receiving response from redr server: " .. vim.inspect(err), vim.log.levels.ERROR)
-      send_message(client, bye_message())
+  local client = uv.new_tcp()
+  client:connect("127.0.0.1", REDR_PORT, function(connect_err)
+    if connect_err then
+      vim.notify("Error connecting to redr server: " .. connect_err, vim.log.levels.ERROR)
       client:close()
       return
     end
 
-    ---@diagnostic disable-next-line: param-type-mismatch
-    local parsed = parse_message(res)
-
-    if parsed == nil or parsed.type ~= "command_ran" then
-      vim.notify("Error parsing response from redr server: " .. vim.inspect(res), vim.log.levels.ERROR)
-      send_message(client, bye_message())
-      client:close()
-      return
-    end
-
-    local exit_code = parsed.exit_code
-
-    if exit_code == 0 then
-      vim.notify("Command `" .. command .. "` ran successfully", vim.log.levels.INFO)
-    else
-      vim.notify("Command `" .. command .. "` failed with exit code " .. exit_code, vim.log.levels.ERROR)
-      if not M.config.run_next_on_failure then
-        send_message(client, bye_message())
+    send_message(client, introduce_message(cwd), function(introduce_res, introduce_err)
+      if introduce_err then
+        vim.notify("Error introducing to redr server: " .. introduce_err, vim.log.levels.ERROR)
         client:close()
         return
       end
-    end
-  end
 
-  send_message(client, bye_message())
-  client:close()
+      local introduce_parsed = parse_message(introduce_res)
+      if introduce_parsed == nil or introduce_parsed.type ~= "ok" then
+        vim.notify("Error parsing response from redr server: " .. vim.inspect(introduce_res), vim.log.levels.ERROR)
+        send_message(client, bye_message(), function()
+          client:close()
+        end)
+        return
+      end
+
+      local function process_command(index)
+        if index > #commands then
+          send_message(client, bye_message(), function()
+            client:close()
+          end)
+          return
+        end
+
+        local command = commands[index]
+        local msg = run_command_message(command)
+
+        send_message(client, msg, function(res, err)
+          if err then
+            vim.notify("Error receiving response from redr server: " .. err, vim.log.levels.ERROR)
+            send_message(client, bye_message(), function()
+              client:close()
+            end)
+            return
+          end
+
+          local parsed = parse_message(res)
+          if parsed == nil or parsed.type ~= "command_ran" then
+            vim.notify("Error parsing response from redr server: " .. vim.inspect(res), vim.log.levels.ERROR)
+            send_message(client, bye_message(), function()
+              client:close()
+            end)
+            return
+          end
+
+          local exit_code = parsed.exit_code
+          if exit_code == 0 then
+            vim.notify("Command `" .. command .. "` ran successfully", vim.log.levels.INFO)
+          else
+            vim.notify("Command `" .. command .. "` failed with exit code " .. exit_code, vim.log.levels.ERROR)
+            if not M.config.run_next_on_failure then
+              send_message(client, bye_message(), function()
+                client:close()
+              end)
+              return
+            end
+          end
+
+          -- Process the next command in the list
+          process_command(index + 1)
+        end)
+      end
+
+      -- Start processing commands from the first one
+      process_command(1)
+    end)
+  end)
 end
 
 return {
