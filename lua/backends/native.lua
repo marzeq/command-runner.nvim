3859a5f3bc1db4197aaab0ba911c9043595646bf
marzeq <marzeqmarzeq@gmail.com>
2025-02-20 19:52:23 +0100

    improve window spawning

--- lua/backends/native.lua
+++ lua/backends/native.lua
@@ -2,20 +2,38 @@
 local M = require("command-runner")
 
 ---@param commands string[]
----@param _ string cwd
-local function run_commands(commands, _)
+---@param cwd string cwd
+local function run_commands(commands, cwd)
   local height = math.ceil(vim.o.lines * (M.config.split_height / 100))
-  local original_splitbelow = vim.api.nvim_get_option("splitbelow")
-  vim.api.nvim_set_option("splitbelow", true)
-  vim.cmd("split")
-  vim.api.nvim_set_option("splitbelow", original_splitbelow)
-  vim.cmd("resize " .. height)
 
-  local buf = vim.api.nvim_create_buf(true, true)
-  vim.api.nvim_set_current_buf(buf)
+  local win = vim.api.nvim_open_win(vim.api.nvim_create_buf(true, true), true, {
+    vertical = true,
+    split = "below",
+    style = "minimal",
+    width = vim.o.columns,
+    height = height,
+  })
 
-  vim.api.nvim_buf_set_keymap(buf, "n", "<ESC>", "<cmd>close<CR>", { noremap = true, silent = true })
-  vim.api.nvim_buf_set_keymap(buf, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
+  local buf = vim.api.nvim_win_get_buf(win)
+  vim.bo[buf].buftype = "nofile"
+  vim.bo[buf].filetype = "commandrunner"
+  vim.bo[buf].swapfile = false
+  vim.bo[buf].buflisted = false
+  vim.bo[buf].bufhidden = "wipe"
+
+  local function close()
+    vim.api.nvim_win_close(win, true)
+  end
+
+  local function open(mode)
+    return "<C-w>" .. mode .. "<C-w>k<cmd>close<CR><C-w>j<cmd>resize " .. height .. "<CR><C-w>k"
+  end
+
+  vim.keymap.set("n", "<ESC>", close, { noremap = true, silent = true, buffer = buf })
+  vim.keymap.set("n", "<CR>", close, { noremap = true, silent = true, buffer = buf })
+  vim.keymap.set("n", "q", close, { noremap = true, silent = true, buffer = buf })
+  vim.keymap.set("n", "gf", open("f"), { noremap = true, silent = true, buffer = buf })
+  vim.keymap.set("n", "gF", open("F"), { noremap = true, silent = true, buffer = buf })
 
   local joiner = M.config.run_next_on_failure and "; " or " && "
   local shell = vim.o.shell
@@ -24,15 +42,14 @@ local function run_commands(commands, _)
     local mapped = vim.tbl_map(function(command)
       return "echo '> " .. command .. "' && " .. command .. " && echo ''"
     end, cmds)
-
     return table.concat(mapped, joiner)
   end
 
-  vim.fn.termopen({ shell, "-c", concat_commands(commands) })
-
-  vim.api.nvim_feedkeys("i", "n", true)
-
-  vim.api.nvim_buf_set_option(buf, "bufhidden", "wipe")
+  vim.fn.termopen({ shell, "-c", concat_commands(commands) }, {
+    on_exit = function()
+      vim.api.nvim_command("stopinsert")
+    end,
+  })
 end
 
 return {
